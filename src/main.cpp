90eed25855fe69f8555f1db0f90a3bc652353b45
Marco Scholl <develop@marco-scholl.de>
2023-01-29 09:48:44 +0100

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "OpenKNX.h"
-// #include "Logic.h"
+#include "Logic.h"
 #include "HardwareModule.h"
 #include "SmartMF.h"
 #include "VirtualButtonModule.h"
@@ -9,7 +9,7 @@ void setup()
     const uint8_t firmwareRevision = 2;
     openknx.init(firmwareRevision);
     smartmf.init();
-    // openknx.addModule(1, new Logic());
+    openknx.addModule(1, new Logic());
     openknx.addModule(2, new VirtualButtonModule());
     openknx.addModule(3, new HardwareModule());
     openknx.setup();
